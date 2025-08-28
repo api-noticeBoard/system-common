db0269c90a4b3ac0242405947bc88313a703a5c4
jimiboy <jimi008285@gmail.com>
2025-08-28 12:47:38 +0900

    Update Errorcode

--- src/main/java/com/portfolio/common/system/exception/ErrorCode.java
+++ src/main/java/com/portfolio/common/system/exception/ErrorCode.java
@@ -22,13 +22,13 @@ public enum ErrorCode {
     // E            Example 예시             Exam을 찾을 수 없습니다.(E001)
 
     /** --- 공통 관련 에러 --- */
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C001", "유효하지 않은 입력 값입니다.: %s"), // 동적 인자(%s)로 입력값 포함
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C001", "유효하지 않은 입력 값입니다. %s"), // 동적 인자(%s)로 입력값 포함
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C002", "지원하지 않는 HTTP 메서드입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C003", "서버 내부 오류가 발생했습니다."),
     INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, "C004", "잘못된 타입의 값입니다."),
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "C005", "접근이 거부되었습니다."),
     /** --- 인증 관련 에러 --- */
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A001", "인증에 실패하였습니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A001", "인증에 실패하였습니다. %s"),
     /** --- 사용자 관련 에러 --- */
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "사용자를 찾을 수 없습니다."),
     /** --- 게시글 관련 에러 --- */
