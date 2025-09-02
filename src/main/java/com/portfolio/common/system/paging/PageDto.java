b25a1ad8ca878aea1b8aeb3f0d9309d505a9659b
jimiboy <jimi008285@gmail.com>
2025-09-02 19:04:57 +0900

    Update paging03

--- src/main/java/com/portfolio/common/system/paging/PageDto.java
+++ src/main/java/com/portfolio/common/system/paging/PageDto.java
@@ -1,5 +1,6 @@
 package com.portfolio.common.system.paging;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,7 +17,9 @@ public class PageDto {
     @Getter
     @Setter
     public static class Request {
-        private int page = 1; // 기본 페이지 번호 (1부터 시작)
+        @Schema(description = "페이지 번호 (1부터 시작)", example = "1", defaultValue = "1")
+        private int page = 1;
+        @Schema(description = "페이지 당 데이터 수", example = "10", defaultValue = "10")
         private int size = 10; // 기본 페이지 크기
 
         /**
@@ -33,6 +36,7 @@ public class PageDto {
          * OFFSET은 건너뛸 데이터의 수를 의미합니다.
          * @return OFFSET 값
          */
+        @Schema(hidden = true)
         public long getOffset() {
             return (long) (page - 1) * size;
         }
