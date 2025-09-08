b9b61d6682b255f3333bcdfff6753ccfb3ca6985
jimiboy <jimi008285@gmail.com>
2025-09-08 19:19:20 +0900

    Update SheetContentsHandlerImpl

--- src/main/java/com/portfolio/common/system/util/ExcelUtils.java
+++ src/main/java/com/portfolio/common/system/util/ExcelUtils.java
@@ -110,8 +110,11 @@ public class ExcelUtils {
             for (Field field : dtoClass.getDeclaredFields()) {
                 if (field.isAnnotationPresent(ExcelColumn.class)) {
                     ExcelColumn annotation = field.getAnnotation(ExcelColumn.class);
-                    field.setAccessible(true); // private 필드에 접근 가능하도록 설정
-                    fieldMap.put(annotation.colIndex(), field);
+                    // colIndex가 -1이 아닌 필드만 맵에 추가
+                    if (annotation.colIndex() >= 0) {
+                        field.setAccessible(true); // private 필드에 접근 가능하도록 설정
+                        fieldMap.put(annotation.colIndex(), field);
+                    }
                 }
             }
         }
