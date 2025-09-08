b7302c45b78014174b2df9cadedb2df432d9e6e1
jimiboy <jimi008285@gmail.com>
2025-09-08 18:12:34 +0900

    Update ExcelColumn

--- src/main/java/com/portfolio/common/system/util/ExcelColumn.java
+++ src/main/java/com/portfolio/common/system/util/ExcelColumn.java
@@ -12,9 +12,9 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME) // 런타임 시에 이 어노테이션 정보를 읽을 수 있어야 합니다.
 public @interface ExcelColumn {
     // 엑셀의 헤더(첫 번째 행)에 표시될 이름을 지정.
-    String headerName();
+    String headerName() default  "";
     // 엑셀 컬럼의 순서를 지정합니다. 낮은 숫자가 앞에 옴.
     int order() default Integer.MAX_VALUE;
     // 업로드 시, 몇 번째 열의 데이터인지 지정 (0부터 시작)
-    int colIndex();
+    int colIndex() default -1;
 }
